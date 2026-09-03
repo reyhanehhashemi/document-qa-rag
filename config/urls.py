b6033a815e826a4606e12af85a279a1f405389a7
Reyhaneh Hajihashemi <reyhanehhashemi171@gmail.com>
2026-09-03 21:40:56 +0330

    chore: bootstrap Django project

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+"""
+URL configuration for the Document QA RAG project.
+"""
+
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
